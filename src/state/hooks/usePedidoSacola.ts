d08b54a235bc68a0b17380a04e409a0f56964ec0
blaubau <86444407+GabrielEvangelista0@users.noreply.github.com>
2022-12-30 14:57:51 -0300

    Update usePedidoSacola.ts

--- src/state/hooks/usePedidoSacola.ts
+++ src/state/hooks/usePedidoSacola.ts
@@ -1,12 +1,23 @@
-import { useSetRecoilState } from "recoil"
+import { useRecoilState, useSetRecoilState } from "recoil"
 import { pratosPedidosState } from "state/atom"
 import typePrato from "interfaces/Pratos"
 import { obterID } from "utils"
+import { IPedido } from "interfaces/Pedido"
 
 export const useAddPedidoSacola = () => {
-    const setPedido = useSetRecoilState(pratosPedidosState)
+    const [pedido, setPedido] = useRecoilState<IPedido[]>(pratosPedidosState)
 
     return (prato: typePrato) => {
-        setPedido(listaAntiga => [{prato: prato, quantidade: 1, id: obterID( )}, ...listaAntiga])
+        setPedido(listaAntiga => [...listaAntiga, { prato: prato, quantidade: 1, id: obterID() }])
+        pedido.forEach(item => {
+            let novalista = [...pedido];
+            if (prato.id == item.prato.id) {
+                if(novalista.length > 1){
+                    novalista.pop()
+                    novalista = [...novalista, { prato: prato, quantidade: 1, id: obterID() }]
+                }
+                setPedido(novalista)
+            }
+        })
     }
 }
